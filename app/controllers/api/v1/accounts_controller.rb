0490c096800e73bf71893ab2313f70da03f79dd7
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-09-09 15:00:01 -0300

    Fix code layout issues

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,51 +1,56 @@
-class Api::V1::AccountsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_account, only: [:show, :update, :destroy]
+# frozen_string_literal: true
 
-  def index
-    @accounts = current_user.accounts
-    render json: @accounts, status: :ok
-  end
+module Api
+  module V1
+    class AccountsController < ApplicationController
+      before_action :authenticate_user!
+      before_action :set_account, only: %i[show update destroy]
 
-  def show
-    render json: @account, status: :ok
-  end
+      def index
+        @accounts = current_user.accounts
+        render json: @accounts, status: :ok
+      end
 
-  def create
-    @account = current_user.accounts.new(account_params)
-    if @account.save
-      render json: @account, status: :created
-    else
-      render_error_message(@account)
+      def show
+        render json: @account, status: :ok
+      end
+
+      def create
+        @account = current_user.accounts.new(account_params)
+        if @account.save
+          render json: @account, status: :created
+        else
+          render_error_message(@account)
+        end
+      end
+
+      def update
+        if @account.update(account_params)
+          render json: @account, status: :ok
+        else
+          render_error_message(@account)
+        end
+      end
+
+      def destroy
+        if @account.destroy
+          render json: { message: 'Account successfully destroyed' }, status: :ok
+        else
+          render_error_message(@account)
+        end
+      end
+
+      private
+
+      def account_params
+        params.require(:account).permit(:name, :description, :initial_amount)
+      end
+
+      def set_account
+        @account = current_user.accounts.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render_unauthorized_message
+      end
     end
-  end
-
-  def update
-    if @account.update(account_params)
-      render json: @account, status: :ok
-    else
-      render_error_message(@account)
-    end
-  end
-
-  def destroy
-    if @account.destroy
-      render json: { message: 'Account successfully destroyed'}, status: :ok
-    else
-      render_error_message(@account)
-    end
-  end
-
-  private
-
-  def account_params
-    params.require(:account).permit(:name, :description, :initial_amount)
-  end
-
-  def set_account
-    @account = current_user.accounts.find(params[:id])
-
-    rescue ActiveRecord::RecordNotFound
-      render_unauthorized_message
   end
 end
