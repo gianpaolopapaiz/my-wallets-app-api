808963ac07822eea5e513ff0714226cc6ddb366a
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2022-11-08 23:32:27 -0300

    Add transactions controller partial specs

---
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::TransactionsController, type: :request do
+  before :each do
+    @user = create(:user)
+    @account = create(:account, user: @user)
+    @account2 = create(:account, name: 'Account 2', user: @user)
+    @category = create(:category, user: @user)
+    post '/users/sign_in', params: { user: { email: @user.email, password: @user.password } }
+  end
+
+  it "INDEX - gets transactions related to the account" do
+    t1 = create(:transaction, account: @account)
+    t2 = create(:transaction, account: @account)
+    create(:transaction, account: @account2)
+
+    get "/api/v1/accounts/#{@account.id}/transactions"
+    parsed_body = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(parsed_body.count).to eq(2)
+    expect(parsed_body.first['id']).to eq(t1.id)
+    expect(parsed_body.last['id']).to eq(t2.id)
+  end
+end
