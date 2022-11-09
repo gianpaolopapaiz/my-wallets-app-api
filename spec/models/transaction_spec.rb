2308c2a78fc77e25ef820b441a735302ef800356
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2022-11-08 22:59:39 -0300

    Add specs

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = create(:user)
+    @account = create(:account, user: @user)
+  end
+
+  context 'validates name' do
+    it 'does not create a transaction without title' do
+      transaction = Transaction.create(title: '',
+                                       description: 'Sample description',
+                                       account: @account)
+
+      expect(transaction.errors.messages[:title][0]).to eq("can't be blank")
+    end
+
+    it 'does not create a transaction without account' do
+      transaction = Transaction.create(title: 'Sample Title',
+                                       description: 'Sample description',
+                                       account: nil)
+
+      expect(transaction.errors.messages[:account_id][0]).to eq("can't be blank")
+    end
+  end
 end
